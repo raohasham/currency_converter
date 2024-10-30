90872ace49b3a4d5b418c75e7873b2ff7ef46ea9
raohasham <hashamrao2005@gmail.com>
2024-10-30 20:31:36 +0500

    mplemented

--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React , { useId } from "react";
 
 function InputBox({
     label,
@@ -6,28 +6,29 @@ function InputBox({
     onAmountChange,
     onCurrencyChange,
     currencyOption =[],
-    selectedCurrency = "usd",
+    selectedCurrency,
     amountDisable = false,
     currencyDisable = false,
     
     className = "",
 }) {
-   
+    
+    const amountInputId = useId()
 
     return (
         <div className={`bg-white p-3 rounded-lg text-sm flex ${className} `}>
             <div className="w-1/2">
-                <label  className="text-black/40 mb-2 inline-block">
+                <label htmlFor={amountInputId} className="text-black/40 mb-2 inline-block">
                     {label}
                 </label>
                 <input
-                    
+                    id={amountInputId}
                     className="outline-none w-full bg-transparent py-1.5"
                     type="number"
                     placeholder="Amount"
                     disabled = {amountDisable}
                     value={amount}
-                    onChange={(e)=>onAmountChange && onAmountChange(number(e.target.value))}
+                    onChange={(e)=>onAmountChange && onAmountChange(Number(e.target.value))}
                 />
             </div>
             <div className="w-1/2 flex flex-wrap justify-end text-right">
--- src/hooks/useCurrencyInfo.js
+++ src/hooks/useCurrencyInfo.js
@@ -1,17 +1,16 @@
 import { useEffect , useState } from "react";
 
 function useCurrencyInfo(currency){
+    const [data , setData] = useState({})
     useEffect(()=>{
-        const [data , setData] = useState({})
-        let yourDate = new Date()
-        yourDate.toISOString().split('T')[0]
+      
         fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
         .then((res)=>res.json())
         .then((res)=>setData(res[currency]))
         .then
     },[currency])
 
-    return data
+    return data;
 
 } 
 export default useCurrencyInfo;
